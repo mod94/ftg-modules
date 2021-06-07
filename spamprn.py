286b5b82d5c8f6294c378ef49e788c75357ebf0e
don_ep1fani0 <45887047+mod94@users.noreply.github.com>
2021-06-07 07:53:28 +0300

    Update spamprn.py

--- spamprn.py
+++ spamprn.py
@@ -12,17 +12,17 @@ async def _(event):
     input_str = event.pattern_match.group(1)
     for i in range(int(input_str)):
         m = await event.respond("https://blyadun.com/uploads/posts/2017-12-11-21/d30145d84a68766d5b8b65155b4e7c75_206279_07big.jpg")
-    if "|" in input_str:
-        counter, spam_text = input_str.split("|")
-        shiiinabot = "\u2060"
-        for i in range(4000):
-            shiiinabot += "\u2060"
-        message_text = shiiinabot + spam_text
-        await event.edit(message_text)
-        for i in range(int(counter)):
-            time.sleep(0.3)
-            await event.respond(message_text)
-        await event.delete()
-    else:
-        await event.edit("отправь сообщение по типу `.spamprn count | spam message` и админы не увидят в недавних сообщениях" +
-                         " \n Вежливо обращаюсь: @shiiinabot")
+        if "|" in input_str:
+            counter, spam_text = input_str.split("|")
+            shiiinabot = "\u2060"
+            for i in range(4000):
+                shiiinabot += "\u2060"
+            message_text = shiiinabot + spam_text
+            await event.edit(message_text)
+            for i in range(int(counter)):
+                time.sleep(0.3)
+                await event.respond(message_text)
+            await event.delete()
+        else:
+            await event.edit("отправь сообщение по типу `.spamprn count | spam message` и админы не увидят в недавних сообщениях" +
+                             " \n Вежливо обращаюсь: @shiiinabot")
