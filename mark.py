2f0acdb3a0696354f03f92da7d03a6e85c0ccf52
don_ep1fani0 <45887047+mod94@users.noreply.github.com>
2021-07-26 19:55:29 +0300

    Create mark.py

---
+++ mark.py
@@ -0,0 +1,26 @@
+import asyncio
+import telethon
+from telethon.tl.types import ChannelParticipantsAdmins
+from .. import loader, utils
+
+
+
+@loader.tds
+class admtagMod(loader.Module):
+    """admtag"""
+    strings = {"name": "admtag"}
+
+    async def markcmd(self, message):
+        """admtag"""
+        args = utils.get_args_raw(message)
+        await message.delete()
+        if not args: return
+        mentions = '<a href="tg://settings">Админы</a>, '
+        counter = 0
+        chat = await message.get_input_chat()
+        async for x in message.client.iter_participants(chat, filter=ChannelParticipantsAdmins):
+            mentions += f'<a href="tg://user?id={x.id}">\u2060</a>'
+            counter += 1
+        if counter == 0: return await message.delete()
+        mentions += args
+        await message.respond(mentions)
